6d84a4ea908e7d24a6b4e71ce33c24c0123aee9f
arya-2407 <aryapofficial@gmail.com>
2024-12-03 21:07:12 -0800

    handling GET and POST for categories

--- sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
+++ sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.sb_ecom;
+package com.ecommerce.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbEcomApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SbEcomApplication.class, args);
 	}
 
