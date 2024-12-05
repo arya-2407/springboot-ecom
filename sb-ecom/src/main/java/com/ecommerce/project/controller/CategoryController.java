379067a447e99ecb8c2bc77e284dec852536b7eb
arya-2407 <aryapofficial@gmail.com>
2024-12-04 19:46:11 -0800

    Implemented category deletions

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,10 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,9 +21,15 @@ public class CategoryController {
         return categoryService.getAllCategories();
     }
 
-    @PostMapping("/api/public/categories")
+    @PostMapping("/api/admin/categories")
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category Added Successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId){
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 }
--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class CategoryServiceImpl implements CategoryService{
 
     private List<Category> categories = new ArrayList<>();
-
+    private long nextId = 0;
     @Override
     public List<Category> getAllCategories() {
         return categories;
@@ -18,6 +18,21 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        nextId++;
+        Long id = nextId;
+        category.setCategoryId(id);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+        if(category == null){
+            return "Category with categoryId : " + categoryId + " does not exist";
+        }
+        categories.remove(category);
+        return "Category with categoryId : " + category.getCategoryId() + " deleted successfully";
     }
 }
