c1386fecb61cd06a5b3044effec2eef5e3a5b412
arya-2407 <aryapofficial@gmail.com>
2024-12-17 17:32:15 -0800

    implemented all services for Product

--- sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -1,9 +1,68 @@
 package com.ecommerce.project.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+import com.ecommerce.project.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(
+            @RequestBody ProductDTO productDTO,
+            @PathVariable Long categoryId){
+        ProductDTO savedProductDTO = productService.addProduct(categoryId,productDTO);
+        return new ResponseEntity<>(savedProductDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/public/products")
+    public ResponseEntity<ProductResponse> getAllProducts(){
+        ProductResponse productResponse = productService.getAllProducts();
+        return new ResponseEntity<>(productResponse,HttpStatus.OK);
+    }
+
+    @GetMapping("/public/categories/{categoryId}/products")
+    public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId){
+        ProductResponse productResponse = productService.getProductsByCategory(categoryId);
+        return new ResponseEntity<>(productResponse,HttpStatus.OK);
+
+    }
+
+    @GetMapping("/public/products/keyword/{productName}")
+    public ResponseEntity<ProductResponse> getProductByName(@PathVariable String productName){
+        ProductResponse productResponse = productService.getProductByName(productName);
+        return new ResponseEntity<>(productResponse,HttpStatus.FOUND);
+    }
+
+    @PutMapping("/admin/products/{productId}")
+    public ResponseEntity<ProductDTO> updateProduct(@RequestBody ProductDTO productDTO,
+                                                    @PathVariable Long productId){
+        ProductDTO updatedProductDTO = productService.updateProduct(productId,productDTO);
+        return new ResponseEntity<>(updatedProductDTO,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/admin/products/{productId}")
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId){
+        ProductDTO deletedProductDTO = productService.deleteProduct(productId);
+        return new ResponseEntity<>(deletedProductDTO,HttpStatus.OK);
+    }
+
+    @PutMapping("/products/{productId}/image")
+    public ResponseEntity<ProductDTO> updateProductImage(
+            @PathVariable Long productId,
+            @RequestParam("image")MultipartFile image) throws IOException {
+        ProductDTO updatedProductDTO = productService.updateProductImage(productId,image);
+        return new ResponseEntity<>(updatedProductDTO,HttpStatus.OK);
+    }
 }
--- sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -1,8 +1,130 @@
-package com.ecommerce.project.service;
+    package com.ecommerce.project.service;
 
-import org.springframework.stereotype.Service;
+    import com.ecommerce.project.exceptions.APIException;
+    import com.ecommerce.project.exceptions.ResourceNotFoundException;
+    import com.ecommerce.project.model.Category;
+    import com.ecommerce.project.model.Product;
+    import com.ecommerce.project.payload.ProductDTO;
+    import com.ecommerce.project.payload.ProductResponse;
+    import com.ecommerce.project.repositories.CategoryRepository;
+    import com.ecommerce.project.repositories.ProductRepository;
+    import org.modelmapper.ModelMapper;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.beans.factory.annotation.Value;
+    import org.springframework.stereotype.Service;
+    import org.springframework.web.multipart.MultipartFile;
 
-@Service
-public class ProductServiceImpl implements ProductService{
+    import java.io.File;
+    import java.io.IOException;
+    import java.nio.file.Files;
+    import java.nio.file.Paths;
+    import java.util.List;
+    import java.util.UUID;
 
-}
+
+    @Service
+    public class ProductServiceImpl implements ProductService{
+
+        @Autowired
+        private ProductRepository productRepository;
+
+        @Autowired
+        private CategoryRepository categoryRepository;
+
+        @Autowired
+        private ModelMapper modelMapper;
+
+        @Autowired
+        private FileService fileService;
+
+        @Value("${project.image}")
+        private String path;
+
+        @Override
+        public ProductDTO addProduct(Long categoryId, ProductDTO productDTO) {
+            Category category = categoryRepository.findById(categoryId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Category","categoryId",categoryId));
+            Product product = modelMapper.map(productDTO, Product.class);
+            product.setCategory(category);
+            product.setImage("default.png");
+            double specialPrice = product.getPrice() -
+                    ((product.getDiscount()*0.01) * product.getPrice());
+            product.setSpecialPrice(specialPrice);
+            Product savedProduct = productRepository.save(product);
+            return modelMapper.map(savedProduct,ProductDTO.class);
+        }
+
+        @Override
+        public ProductResponse getAllProducts() {
+            List<Product> productList = productRepository.findAll();
+            List<ProductDTO> productDTOS =  productList.stream()
+                    .map(product -> modelMapper.map(product,ProductDTO.class))
+                    .toList();
+            ProductResponse productResponse = new ProductResponse();
+            productResponse.setContent(productDTOS);
+            return productResponse;
+        }
+
+        @Override
+        public ProductResponse getProductsByCategory(Long categoryId) {
+            Category category = categoryRepository.findById(categoryId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Category","categoryId",categoryId));
+            List<Product> products = productRepository.findByCategory(category);
+            List<ProductDTO> productDTOS = products.stream()
+                    .map(product -> modelMapper.map(product,ProductDTO.class))
+                    .toList();
+            ProductResponse productResponse = new ProductResponse();
+            productResponse.setContent(productDTOS);
+            return productResponse;
+        }
+
+        @Override
+        public ProductResponse getProductByName(String productName) {
+            List<Product> products = productRepository.findByProductNameLikeIgnoreCase('%' + productName + '%');
+            List<ProductDTO> productDTOS = products.stream()
+                    .map(product -> modelMapper.map(product,ProductDTO.class))
+                    .toList();
+            ProductResponse productResponse = new ProductResponse();
+            productResponse.setContent(productDTOS);
+            return productResponse;
+        }
+
+        @Override
+        public ProductDTO updateProduct(Long productId, ProductDTO productDTO) {
+            Product existingProduct = productRepository.findById(productId).
+                    orElseThrow(() -> new APIException("Product does not exist"));
+            Product product = modelMapper.map(productDTO, Product.class);
+            existingProduct.setProductName(product.getProductName());
+            existingProduct.setDescription(product.getDescription());
+            existingProduct.setQuantity(product.getQuantity());
+            existingProduct.setPrice(product.getPrice());
+            existingProduct.setDiscount(product.getDiscount());
+            double specialPrice = product.getPrice() -
+                    ((product.getDiscount()*0.01) * product.getPrice());
+            existingProduct.setSpecialPrice(specialPrice);
+            Product savedProduct = productRepository.save(existingProduct);
+            return modelMapper.map(savedProduct,ProductDTO.class);
+        }
+
+        @Override
+        public ProductDTO deleteProduct(Long productId) {
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new APIException("Product not found"));
+            productRepository.deleteById(productId);
+            return modelMapper.map(product,ProductDTO.class);
+        }
+
+        @Override
+        public ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException {
+            Product productFromDb = productRepository.findById(productId)
+                                                .orElseThrow(()->new APIException("Product not found"));
+
+            String fileName = fileService.uploadImage(path,image);
+            productFromDb.setImage(fileName);
+            Product savedProduct = productRepository.save(productFromDb);
+            return modelMapper.map(savedProduct,ProductDTO.class);
+
+        }
+
+
+    }
