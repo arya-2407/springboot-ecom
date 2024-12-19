2dc9be38f459423ceb9396e08f695dafe21c3e25
arya-2407 <aryapofficial@gmail.com>
2024-12-19 14:40:03 -0800

    Configured sign in endpoint

--- sb-ecom/src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ sb-ecom/src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -1,14 +1,20 @@
-package com.ecommerce.project.security.jwt;
+package com.ecommerce.project.security.response;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-public class LoginResponse {
+public class UserInfoResponse {
+    @Getter
+    @Setter
+    private Long id;
     private String jwtToken;
-
     private String username;
     private List<String> roles;
 
-    public LoginResponse(String username, List<String> roles, String jwtToken) {
+    public UserInfoResponse(Long id,String username, List<String> roles, String jwtToken) {
+        this.id = id;
         this.username = username;
         this.roles = roles;
         this.jwtToken = jwtToken;
