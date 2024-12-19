871df79003a19cd7d7bcdbd10a6df2cf91604070
arya-2407 <aryapofficial@gmail.com>
2024-12-19 12:36:48 -0800

    Defined User-Adress  relationship

--- sb-ecom/src/main/java/com/ecommerce/project/model/Role.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Role.java
@@ -1,11 +1,13 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Table(name = "roles")
 public class Role {
