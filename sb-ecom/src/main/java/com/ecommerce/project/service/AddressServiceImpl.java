cc753aa68c0dbcc04a0fedd7e1cab5e0a7f5553c
arya-2407 <aryapofficial@gmail.com>
2024-12-21 10:14:34 -0800

    Adding addresses

---
+++ sb-ecom/src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.model.Address;
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.payload.AddressDTO;
+import com.ecommerce.project.repositories.AddressRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService{
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    @Override
+    public AddressDTO createAddress(AddressDTO addressDTO, User user) {
+        Address address = modelMapper.map(addressDTO,Address.class);
+        List<Address> addressList = user.getAddresses();
+        if (addressList.contains(address)){
+            throw new APIException("Address Already Saved Previously");
+        }
+        addressList.add(address);
+        user.setAddresses(addressList);
+
+        address.setUser(user);
+        Address savedAddress = addressRepository.save(address);
+
+        return modelMapper.map(address,AddressDTO.class);
+    }
+}
